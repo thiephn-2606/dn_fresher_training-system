09e78df25f7ede225370d3933351a76c97362413
thiephn-2606 <huynh.nhat.thiep@sun-asterisk.com>
2021-07-26 21:05:25 +0700

    create courses add trainee and add subject

--- training_system/app/helpers/courses_helper.rb
+++ training_system/app/helpers/courses_helper.rb
@@ -2,4 +2,12 @@ module CoursesHelper
   def status_select
     Course.statuses.keys
   end
+
+  def subject_select
+    Subject.select(:id, :name)
+  end
+
+  def user_select
+    User.select(:id, :name)
+  end
 end
--- training_system/app/javascript/packs/application.js
+++ training_system/app/javascript/packs/application.js
@@ -3,12 +3,14 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("jquery")
+require("select2")
 
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "bootstrap"
+import "packs/courses"
 
 Rails.start()
 Turbolinks.start()
