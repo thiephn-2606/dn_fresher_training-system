b4c1c224b76b087b8d1a8769140055dca42d9498
thiephn-2606 <huynh.nhat.thiep@sun-asterisk.com>
2021-08-05 09:27:49 +0700

    trainee_start_subject

--- training_system/app/models/course_subject.rb
+++ training_system/app/models/course_subject.rb
@@ -2,4 +2,6 @@ class CourseSubject < ApplicationRecord
   belongs_to :course
   belongs_to :subject
   enum status: {init: 0, in_progress: 1, finished: 2}
+  scope :course_subject_start, ->(course_id){where(course_id: course_id)}
+  scope :created_asc, ->{order :created_at}
 end
--- training_system/config/routes.rb
+++ training_system/config/routes.rb
@@ -14,5 +14,6 @@ Rails.application.routes.draw do
 
   namespace :trainee do
     resources :courses
+    resources :course_subjects, only: :update
   end
 end
