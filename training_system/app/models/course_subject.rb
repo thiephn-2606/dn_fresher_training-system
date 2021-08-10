6d270abdcd43a09333f20268951ebde36dbb69fe
ducth-2598 <thai.hong.duc@sun-asterisk.com>
2021-08-10 15:12:37 +0700

    trainer_start_course

--- training_system/app/models/course_subject.rb
+++ training_system/app/models/course_subject.rb
@@ -1,6 +1,10 @@
 class CourseSubject < ApplicationRecord
   belongs_to :course
   belongs_to :subject
+  has_many :user_course_subjects, dependent: :destroy
+  has_many :course_subjects, through: :user_course_subjects
+  
+  delegate :name, :description, :duration, to: :subject
   enum status: {init: 0, in_progress: 1, finished: 2}
   scope :course_subject_start, ->(course_id){where(course_id: course_id)}
   scope :created_asc, ->{order :created_at}
